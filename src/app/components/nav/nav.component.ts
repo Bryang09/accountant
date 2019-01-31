43eb8c1b12327b718a00aa201f2253b42e49685e
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-01-31 07:57:01 -0600

    Burger Nav Added

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Nav } from "./nav.model";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  selector: "app-nav",
+  templateUrl: "./nav.component.html",
+  styleUrls: ["./nav.component.scss"]
 })
 export class NavComponent implements OnInit {
+  navs: Nav[] = [
+    new Nav("About", true, "about"),
+    new Nav("Careers", true, "careers"),
+    new Nav("Services", true, "services"),
+    new Nav("Contact", false, "contact")
+  ];
 
-  constructor() { }
+  burger: boolean = false;
+
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.navs);
+    console.log(this.burger);
   }
 
+  onBurger() {
+    this.burger = !this.burger;
+    console.log(this.burger);
+  }
 }
---
+++ src/app/components/nav/nav.model.ts
@@ -0,0 +1,7 @@
+export class Nav {
+  constructor(
+    public text: string,
+    public normal: boolean,
+    public link: string
+  ) {}
+}
